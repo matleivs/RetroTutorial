b15f49b455bffcd0e130161848ad3d1f43bec08f
lei <matleivs@gmail.com>
2018-03-11 19:41:00 +0100

    GamePanel: add collision detection and game over option --> screen stops when player collides with obstacle, user can reset on MotionEvent ACTION_UP

--- app/src/main/java/com/apps/matlei/retrotutorial/GamePanel.java
+++ app/src/main/java/com/apps/matlei/retrotutorial/GamePanel.java
@@ -3,6 +3,7 @@ package com.apps.matlei.retrotutorial;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.view.MotionEvent;
@@ -16,10 +17,17 @@ import com.apps.matlei.retrotutorial.gameobjects.RectPlayer;
  */
 class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
+    private Rect r = new Rect();
 
     private RectPlayer player;
     private Point playerPoint;
     private ObstacleManager obstacleManager;
+
+    private boolean movingPlayer = false;
+
+    private boolean gameOver = false;
+    private long gameOverTime;
+
 
     public GamePanel(Context context) {
         super(context);
@@ -30,7 +38,11 @@ class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
         // instantiate Player; Color.rgb = static class, takes in the red, green, and blue value of a color & convert to corresponding integer
         player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
-        playerPoint = new Point(150, 150);
+        // start position of the player: middle of the screen's width, and 3 /4 down of the screen's height
+        playerPoint = new Point(Constants.SCREEN_WIDTH/2,3*Constants.SCREEN_HEIGHT/4);
+        // to align the player to the playerPoint:
+        player.update(playerPoint);
+
 
         obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
         // now we need to handle the portion in which this point changes position corresponding to where we touch
@@ -60,6 +72,7 @@ class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
             try {
                 // stop our game loop
                 thread.setRunning(false);
+
                 // finish running the thread and terminate it
                 thread.join();
             } catch (Exception e) {
@@ -70,17 +83,31 @@ class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    public boolean onTouchEvent(MotionEvent motionEvent) {
         // manage our touch inputs
         // switch case all the different Action Types:
-        switch (event.getAction()) {
-            // case without breaking: we want both cases feed into the same thing
+        switch (motionEvent.getAction()) {
+            // formerly: case without breaking: we want both cases feed into the same thing
             case MotionEvent.ACTION_DOWN:
+                if (!gameOver && player.getRectangle().contains((int) motionEvent.getX(), (int) motionEvent.getY())) {
+                    movingPlayer = true;
+                }
+                // show game over screen for 2 secs, before user can tap on screen and set gameover to false & reset game
+//                if(gameOver && System.currentTimeMillis() - gameOverTime >= 2000){ <-- WIP this is not working, the app freezes at game over screen and never resets
+                if(gameOver){
+                    reset();
+                }
+                break;
             case MotionEvent.ACTION_MOVE:
-                playerPoint.set((int) event.getX(), (int) event.getY()); // just set the player to where the finger touched the screen
-
+                if (!gameOver && movingPlayer) {
+                    // set the player to where the finger touched the screen
+                    playerPoint.set((int) motionEvent.getX(), (int) motionEvent.getY()); 
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                movingPlayer = false;
+                break;
         }
-
 
         return true; // returning false might give the app reasons not to detect our touch event,
         // but we just want to detect Every touch, so we just return True.
@@ -88,12 +115,31 @@ class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     /**
+     * Reset all the obstacles and move our player to a safe starting position.
+     */
+    private void reset() {
+        //reset position of player back into starting position
+        playerPoint = new Point(Constants.SCREEN_WIDTH/2, 3*Constants.SCREEN_HEIGHT/4);
+        player.update(playerPoint);
+        obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
+        movingPlayer = false;
+        gameOver = false;
+    }
+
+    /**
      * Update our game frame by frame,
-     * add 2 to y & x value of the gameobject (game character)
+     * update the position of our gameobject (game character)
+     * and update the positions of our obstacles = moving them down
      */
     public void update() {
-        player.update(playerPoint); // playerPoint = where it should be
-        obstacleManager.update();
+        if (!gameOver) {
+            player.update(playerPoint); // playerPoint = where it should be
+            obstacleManager.update();
+        }
+        if(obstacleManager.playerCollide(player)){
+            gameOver = true;
+            gameOverTime = System.currentTimeMillis();
+        }
     }
 
     /**
@@ -105,13 +151,35 @@ class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-
         // fill the screen with color --> make the canvas solid yellow except for our player then
         canvas.drawColor(Color.YELLOW);
 
         // draw the player on the screen
         player.draw(canvas);
+        // draw the obstacles on the screen
         obstacleManager.draw(canvas);
+
+        // on GameOver, we wanna draw a "game over" text on the screen
+        if(gameOver){
+            Paint paint = new Paint();
+            paint.setTextSize(100);
+            paint.setColor(Color.MAGENTA);
+            drawCenterText(canvas, paint, getContext().getResources().getString(R.string.gameOver));
+        }
+    }
+
+
+    // andreas1724 (white color): see {@link https://stackoverflow.com/questions/11120392/android-center-text-on-canvas}
+    private void drawCenterText(Canvas canvas, Paint paint, String text) {
+        paint.setTextAlign(Paint.Align.LEFT);
+//        paint.setColor(Color.rgb(255, 255, 255));
+        canvas.getClipBounds(r);
+        int cHeight = r.height();
+        int cWidth = r.width();
+        paint.getTextBounds(text, 0, text.length(), r);
+        float x = cWidth / 2f - r.width() / 2f - r.left;
+        float y = cHeight / 2f + r.height() / 2f - r.bottom;
+        canvas.drawText(text, x, y, paint);
     }
 
 }
