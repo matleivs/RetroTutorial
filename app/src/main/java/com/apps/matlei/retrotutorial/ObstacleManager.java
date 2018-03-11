a100994a433be1a5a36c26161c0d2aa471800dd5
lei <matleivs@gmail.com>
2018-03-11 15:57:32 +0100

    ObstacleManager: fix arrayindexoutofbounds error in while condition, fix adding new and removing old obstacles.
     Note: the obstacle with the highest index: if its top is below the Screen, then
                => generate a new one which is right At the top of the screen,
                => remove the obstacle with the highest index from the arraylist.

--- app/src/main/java/com/apps/matlei/retrotutorial/ObstacleManager.java
+++ app/src/main/java/com/apps/matlei/retrotutorial/ObstacleManager.java
@@ -46,13 +46,13 @@ public class ObstacleManager {
 
     /**
      * The region we fill with obstacles has to be larger than our screen height, four thirds of it.
-     * The larger the index => the further down on the screen the obstacle is.
+     * The larger the index in the list => the further down on the screen the obstacle is.
      */
     private void populateObstacles() {
         int currY = -5 * Constants.SCREEN_HEIGHT / 4;
         // keep generateing obstacles  as long as it has not gone onto the screen yet
         // also generate a "random" gap for the player to go thru everytime => random x value for that start
-        while (obstacles.get(obstacles.size() - 1).getRectangle().bottom < 0) {
+        while (currY < 0) {
             // make sure there is room on the right of the random xValue where the player can fit in: generate random start-x
             int xStart = (int) (Math.random() * (Constants.SCREEN_WIDTH - playerGap));
             obstacles.add(new Obstacle(obstacleHeight, color, xStart, currY, playerGap));
@@ -62,6 +62,7 @@ public class ObstacleManager {
 
     /**
      * Generate and add obstacles to the screen based on time = frame-rate independent.
+     * Note: the highest index: if its top is below the Screen  => then generate a new one which is right At the top of the screen.
      */
     public void update() {
         int elapsedTime = (int) (System.currentTimeMillis() - startTime);
@@ -75,8 +76,14 @@ public class ObstacleManager {
         if (obstacles.get(obstacles.size() - 1).getRectangle().top >= Constants.SCREEN_HEIGHT) {
             // generate a new start-x
             int xStart = (int) (Math.random() * (Constants.SCREEN_WIDTH - playerGap));
-            // Note: the obstacle at index 0 is the highest on screen (lower index => higer on screen)
-            obstacles.add(0, new Obstacle(obstacleHeight, color, xStart, obstacles.get(0).getRectangle().top + obstacleHeight + obstacleGap, playerGap));
+            // Note: the obstacle at index 0 is the highest on screen (lower index => higher on screen)
+
+            // Note: the obstacle with the highest index: if its top is below the Screen, then
+            // => generate a new one which is right At the top of the screen,
+            // => remove the obstacle with the highest index from the arraylist.
+
+            obstacles.add(0, new Obstacle(obstacleHeight, color, xStart, obstacles.get(0).getRectangle().top - obstacleHeight - obstacleGap, playerGap));
+            obstacles.remove(obstacles.size() - 1);
         }
     }
 
